ae57337e1ac35cd3d0b33dfd507d66fdb2840845
Rawan <rawan.shisht@gmail.com>
2019-07-02 20:24:42 +0200

    Problem solved

--- graphInput.py
+++ graphInput.py
@@ -1,7 +1,7 @@
 import pickle 
 nodes={} #{ "N2":Node}
 nodeCounter = 1
-graphFile = open('graphData', 'rb') 
+graphFile = open('graphData2.txt', 'rb') 
 nodes = pickle.load(graphFile) 
 nodeCounter = len(nodes.keys()) + 1
 print(nodeCounter)
@@ -20,7 +20,8 @@ while True:
     x, y = map(int, input().split())
     nodePostion=(x, y)
     # print(nodePostion)
-    nodeChildren=[]
+    directions={}
+    nodeChildren= []
     nodeChildren = list(map(str, input("Enter Node's Children:").split())) 
     print("List of students: ", nodeChildren)
     print(len(nodeChildren))
@@ -49,7 +50,7 @@ while True:
     nodes[nodeName]=nodeData
 
 # Its important to use binary mode 
-graphFile = open('graphData', 'wb') 
+graphFile = open('graphData2.txt', 'wb') 
 # source, destination 
 # print(nodes['N2'])
 pickle.dump(nodes, graphFile)                      
